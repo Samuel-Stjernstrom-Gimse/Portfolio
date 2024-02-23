7b340f4cb721d68b298ec351e42ce1895412e00b
Zam <samuel.kodehode@gmail.com>
2024-02-23 23:00:37 +0100

    1.3.0

---
+++ projects.js
@@ -0,0 +1,6 @@
+"use strict";
+const scrollArrow = document.getElementById('scroll-arrow');
+scrollArrow.addEventListener('click', () => {
+    window.scroll(0, 700);
+});
+//# sourceMappingURL=projects.js.map
